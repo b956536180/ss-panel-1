501c23d6594080b064f968662263a0f65ed6940c
BadApple <1076351865@qq.com>
2016-10-10 23:32:33 +0800

    更新finalspeed
    更新finalspeed

--- sspanel/user/_reg.php
+++ sspanel/user/_reg.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../lib/config.php';
+include_once("smtp.class.php");
 $email = $_POST['email'];
 $email = strtolower($email);
 $passwd = $_POST['passwd'];
@@ -34,8 +35,31 @@ if(!$code->IsCodeOk()){
     //do reg
     $reg = new \Ss\User\Reg();
     $reg->Reg($name,$email,$passwd,$plan,$transfer,$invite_num,$ref_by);
-    $code->Del();
+    //开放注册
+    //$code->Del();
+    //邮箱验证
+    /*$smtpserver = "smtp.qq.com"; //SMTP服务器
+    $smtpserverport = 25; //SMTP服务器端口
+    $smtpusermail = "1076351865@qq.com"; //SMTP服务器的用户邮箱
+    $smtpuser = "1076351865@qq.com"; //SMTP服务器的用户帐号
+    $smtppass = "XBWxbw12138"; //SMTP服务器的用户密码
+    $smtp = new Smtp($smtpserver, $smtpserverport, false, $smtpuser, $smtppass); //这里面的一个true是表示使用身份验证,否则不使用身份验证.
+    $emailtype = "HTML"; //信件类型，文本:text；网页：HTML
+    $smtpemailto = $email;
+    $smtpemailfrom = $smtpusermail;
+    $emailsubject = "用户帐号激活";
+    $token='12138';
+    $emailbody = "亲爱的".$name."：<br/>感谢您在我站注册了新帐号。<br/>请点击链接激活您的帐号。<br/><a href='http://ecfun.cc/mvp/user/active.php?verify=".$token."' target='_blank'>http://ecfun.cc/mvp/user/active.php?verify=".$token."</a><br/>如果以上链接无法点击，请将它复制到你的浏览器地址栏中进入访问，该链接24小时内有效。<br/>如果此次激活请求非你本人所发，请忽略本邮件。<br/><p style='text-align:right'>--------v.ecfun.cc 敬上</p>";
+    $rs = $smtp->sendmail($smtpemailto, $smtpemailfrom, $emailsubject, $emailbody, $emailtype);
+    if($rs==1){
+        $a['ok'] = '1';
+        $a['msg'] = "恭喜您，注册成功！<br/>请登录到您的邮箱及时激活您的帐号！";
+    }else{
+        $a['ok'] = '0';
+        $a['msg'] = $rs;
+    }*/
     $a['ok'] = '1';
     $a['msg'] = "注册成功";
+    
 }
 echo json_encode($a);
